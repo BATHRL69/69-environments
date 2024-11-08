3d2f2a2b4966fb7026cf155696c1279527a3667a
Solomon Wheeler <solly.wheeler@gmail.com>
2024-11-08 17:41:12 +0000

    PPO skeleton

---
+++ Walker/ppo.py
@@ -0,0 +1,23 @@
+## TODO
+# Skeleton code - Solly
+#
+#
+#
+
+import gymnasium as gym
+import numpy as np
+import random
+
+
+class PPOAgent:
+    def __init__(
+        self,
+        env,
+    ):
+        self.env = env
+
+    def train():
+        pass
+
+    def predict():
+        pass
