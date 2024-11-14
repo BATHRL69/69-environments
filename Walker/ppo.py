feca31e69e625835b47f0a60d244105edfa618e1
Solomon Wheeler <solly.wheeler@gmail.com>
2024-11-14 14:21:00 +0000

    Start on PPO networks

--- Walker/ppo.py
+++ Walker/ppo.py
@@ -12,40 +12,92 @@ import pickle
 import os
 import torch
 import torch.nn as nn
-
+from torch.distributions import Normal
 from enum import Enum
 from ppo_constants import *
 
 
 class PPOPolicyNetwork(nn.Module):
-    """This is sometimes also called the ACTOR network
+    """This is sometimes also called the ACTOR network.
+    Basically, the goal of this network is to take in a state, and give us the best possible actions to take in that state.
+    We will do this with a neural net that takes in the observation space, and outputs action space values.
 
     Args:
         nn (_type_): _description_
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        observation_space=105,  # Defaults set from ant walker
+        action_space=8,  # Defaults set from ant walker
+    ):
+        # TODO need to work out what network is going to work best here
+        # TODO need to workout what size of layer works best#
+
+        self.network = nn.Sequential(
+            nn.Linear(observation_space, 32),
+            nn.Tanh(),
+            nn.linear(32, action_space),
+            nn.Tanh(),  # Ant action space is -1 to 1
+        )
+
+    def action(self, state):
+        """Given a state, gets an action, sampled from normal distribution
+
+        Args:
+            state (torch.tensor): Current state (observation space)
+
+        Returns:
+            torch.tensor: action, probability
+        """
+        action_values = self.network(state)
+        distribution = Normal(action_values, 1.0)  # Std of 1.0
+        action = distribution.sample()
+        probability = distribution.log_prob(action)
+        return action, probability
+
+    def evaluate(self, state, action):
         pass
+
+    def forward(self, state):
+        raise NotImplementedError()
+        return self.network(state)
 
 
 class PPOValueNetwork(nn.Module):
-    """This is sometimes also called the CRITIC network
+    """This is sometimes also called the CRITIC network.
+    Basically, this network will give us the predicted return for a single state (observation space), given all of our observations
 
     Args:
         nn (_type_): _description_
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        observation_space=105,  # Defaults set from ant walker
+    ):
+        self.network = nn.Sequential(
+            nn.Linear(observation_space, 32),
+            nn.Tanh(),
+            nn.Linear(32, 32),
+            nn.Tanh(),
+            nn.linear(32, 1),
+        )
 
 
 class PPOAgent(Agent):
-    def __init__(self, env, epsilon=0.001, gamma=0.99):
+    def __init__(
+        self,
+        env,
+        epsilon=0.001,
+        gamma=0.99,
+    ):
         self.policy_network = None
         self.value_network = None
         self.env = env
         self.epsilon = epsilon  # How large of a step we will take with updates
         self.gamma = gamma  # Discount factor
+        self.optimizer = torch.optim
 
     def probability_ratios(self, state, action):
         """Calculate our probability ratio, which is:
@@ -98,7 +150,8 @@ class PPOAgent(Agent):
     def ppo_clip(self, state, action):
         pass
 
-    def train(self):
+    def simulate_episode(self):
+        """Simulate a single episode, called by train method on parent class"""
         pass
 
     def predict(self):
