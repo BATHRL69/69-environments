aee4ce0e6bd5e13b5c076eb600cc0072ac67f329
Solomon Wheeler <solly.wheeler@gmail.com>
2024-12-18 16:01:22 +0000

    Added PPO plots

--- Walker/plots.py
+++ Walker/plots.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-loaded_timesteps = np.load('sac_timesteps_mean.npy')[1:]
-loaded_rewards = np.load('sac_rewards_mean.npy')[1:]
+loaded_timesteps = np.load("ppo_timesteps_mean.npy")[1:]
+loaded_rewards = np.load("ppo_rewards_mean.npy")[1:]
+# loaded_timesteps = np.load('sac_timesteps_mean.npy')[1:]
+# loaded_rewards = np.load('sac_rewards_mean.npy')[1:]
 
 plt.figure(figsize=(10, 6))
-plt.plot(loaded_timesteps, loaded_rewards, label='Reward', color='blue')
-plt.xlabel('Timesteps')
-plt.ylabel('Rewards')
-plt.title('Reward Over Timesteps')
+plt.plot(loaded_timesteps, loaded_rewards, label="Reward", color="blue")
+plt.xlabel("Timesteps")
+plt.ylabel("Rewards")
+plt.title("Reward Over Timesteps")
 plt.legend()
 plt.grid()
 plt.show()
-
